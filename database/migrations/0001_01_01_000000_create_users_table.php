a6cf8e0908c75d0ed7204879a66cc1500488fa21
joel-2112 <eyueljoel21@gmail.com>
2025-03-02 22:55:19 +0300

    the user model and migration table done

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->enum('role', ['customer', 'driver', 'admin'])->default('customer');
             $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
